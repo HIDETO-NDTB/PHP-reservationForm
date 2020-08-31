d44023c16a01426fa7630575f5ef1c90211b208e
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-08-31 19:48:28 +0900

    set favicon

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>予約フォーム | <?php echo $siteTitle; ?></title>
-        
+        <link rel="shortcut icon" href="img/reservation.png">
         <link rel="stylesheet" href="style.css" type="text/css">
     </head>
     
