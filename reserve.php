a2f7fc18d519fbf788b6f827cbca08ca228e0278
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-09-01 16:15:40 +0900

    add sanitize

--- reserve.php
+++ reserve.php
@@ -4,10 +4,10 @@
 
   debug('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 予約ページ');
 
-  if(!isLogin()){
+  if (!isLogin()) {
       debug('ログインチェック：未ログインユーザーなのでログインページに遷移します');
       header("Location:login.php");
-  }else{
+  } else {
       debug('ログインチェック：ログインユーザーでした。');
   }
 
@@ -20,25 +20,23 @@
 
   debug('予約時間：'.$rsv_date);
 
-  if(!empty($_POST['reservation'])){
-      
+  if (!empty($_POST['reservation'])) {
       debug('予約のPOST送信がありました。');
       
-      try{
+      try {
           $dbh = dbConnect();
           $sql = 'INSERT INTO reservations (shop_id, menu_id, user_id, reservation_date) VALUES(:s_id, :m_id, :u_id, :date)';
           $data = array(':s_id' => $_GET['s_id'], ':m_id' => $_GET['m_id'], ':u_id' => $_SESSION['login_user'], ':date' => $rsv_date);
           
           $stmt = queryPost($dbh, $sql, $data);
           
-          if($stmt){
+          if ($stmt) {
               $_SESSION['suc_msg'] = SUC_MSG06;
               header("Location:index.php");
-          }else{
+          } else {
               $err_msg['common'] = ERR_MSG16;
           }
-          
-      }catch(Evception $e){
+      } catch (Evception $e) {
           error_log('エラー発生：'.$e->getMessage());
       }
   }
@@ -55,7 +53,9 @@
 <main class="sub-container">
     <form action="" method="post">
         
-        <h3 class="card-header">予約確認<span class="err-area"><?php if(!empty($err_msg['common'])){ echo $err_msg['common'];} ?></span></h3>
+        <h3 class="card-header">予約確認<span class="err-area"><?php if (!empty($err_msg['common'])) {
+    echo $err_msg['common'];
+} ?></span></h3>
         
         <h4 class="sub-header">予約情報</h4>
          
@@ -72,7 +72,7 @@
         <h4 class="sub-header">会員情報</h4>
         
         <h5 class="title">ログインID</h5>
-        <h6 class="confirm"><?php echo $_SESSION['user_id']; ?></h6>
+        <h6 class="confirm"><?php echo $_SESSION['login_user']; ?></h6>
         
         <div class="btn-area">
         
